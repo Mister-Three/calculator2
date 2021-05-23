e032022302bfb11561c409a882d6f31c225d2356
Mister-Three <81454774+Mister-Three@users.noreply.github.com>
2021-05-22 20:30:14 -0400

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,52 @@
+import math
+import time
+print("Hello, what shall you be refered to as?")
+user_name = input()
+print("Hello, " + user_name)
+print("Welcome to M3r_\'s math problem solver")
+time.sleep(1)
+print("+, -, *, /, **, pi, help")
+time.sleep(1)
+while True:
+    calc = input("What type of equation are we going today?\n")
+    if calc == '+':
+        ask1 = input("Input the first number: ")
+        ask2 = input("Input the second number: ")
+        ask1 = int(ask1)
+        ask2 = int(ask2)
+        print(ask1 + ask2)
+    if calc == '-':
+        ask1 = input("Input the first number: ")
+        ask2 = input("Input the second number: ")
+        ask1 = int(ask1)
+        ask2 = int(ask2)
+        print(ask1 - ask2)
+    if calc == '*':
+        ask1 = input("Input the first number: ")
+        ask2 = input("Input the second number: ")
+        ask1 = int(ask1)
+        ask2 = int(ask2)
+        print(ask1 * ask2)
+    if calc == '/':
+        ask1 = input("Input the first number: ")
+        ask2 = input("Input the second number: ")
+        ask1 = float(ask1)
+        ask2 = float(ask2)
+        print(ask1 / ask2)
+    if calc == '**':
+        ask1 = input("Input the first number: ")
+        ask2 = input("Input the second number: ")
+        ask1 = int(ask1)
+        ask2 = int(ask2)
+        print(ask1 ** ask2)
+    if calc == 'pi':
+        print(math.pi)
+    if calc == 'help':
+        print("+, -, *, /, pi, help")
+        time.sleep(1)
+    close = input("Do you want to close this tab? Y/N \n")
+    if close == 'Y':
+        exit()
+    elif close == 'N':
+        print("Continuing...")
+        time.sleep(1)
